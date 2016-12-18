b3abdb72203383b49ba14bfd62dfdd59c1e780d4
Saloni Mittal <iec2014071@iiita.ac.in>
2016-12-19 01:02:02 +0530

    Add files via upload

---
+++ libraries.py
@@ -0,0 +1,74 @@
+import numpy as np
+from skimage import color, exposure, transform,io,data
+from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Activation, Flatten,Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.models import model_from_json,Sequential
+import h5py
+from PIL import Image
+import cv2
+import numpy as np 	
+import io
+
+#from tkinter import *
+import skimage
+from sklearn.externals import joblib
+from skimage.feature import hog 
+import matplotlib
+import imutils
+import copy
+from joblib import Parallel, delayed
+
+
+import skimage
+import cv2
+import numpy as np
+from sklearn.externals import joblib
+from skimage import data, color, exposure,io
+from skimage.feature import hog 
+import matplotlib
+import imutils
+import copy
+from joblib import Parallel, delayed
+import glob
+import skimage
+import cv2
+import numpy as np
+import glob
+from sklearn.externals import joblib
+from skimage import data, color, exposure, io
+from skimage.feature import hog 
+import matplotlib
+import imutils
+import copy
+from joblib import Parallel, delayed
+import numpy as np
+from skimage import color, exposure, transform
+from keras.utils import np_utils
+from skimage import io
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.models import model_from_json
+import h5py
+from PIL import Image
+import PIL
+import cv2
+import numpy as np 	
+import io
+from PIL import Image
+
+import skimage
+import cv2
+import numpy as np
+import glob
+from sklearn.externals import joblib
+from skimage import data, color, exposure,io
+from skimage.feature import hog 
+import matplotlib
+import imutils
+import lib2to3
+import copy
+import time
+from joblib import Parallel, delayed
